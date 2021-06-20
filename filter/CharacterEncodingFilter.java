63251a91a06d3d71a70152a64b5571a413056ff2
apeach0710 <74312243+apeach0710@users.noreply.github.com>
2021-06-21 00:05:43 +0900

    Add files via upload

--- filter/CharacterEncodingFilter.java
+++ filter/CharacterEncodingFilter.java
@@ -1,4 +1,4 @@
-package filter;
+package com;
 
 import java.io.IOException;
 
@@ -8,14 +8,13 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.annotation.WebInitParam;
 
-@WebFilter("/*")
+@WebFilter("/*")	
 public class CharacterEncodingFilter implements Filter {
-
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        request.setCharacterEncoding("utf-8");
-        response.setContentType("text/html;charset=utf-8");
-        chain.doFilter(request, response);
-    }
+   
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {		
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		chain.doFilter(request, response);
+	}
 }
