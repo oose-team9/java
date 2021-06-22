2a2bcbc76c56585e785578f472d8837f408eb037
dyddn3994 <69083407+dyddn3994@users.noreply.github.com>
2021-06-22 19:36:21 +0900

    Add files via upload

---
+++ domain/CardInformation.java
@@ -0,0 +1,33 @@
+package domain;
+
+public class CardInformation {
+	private int id;
+	private int employeeNo;
+	private int cardNumber;
+	private String bank;
+	private int accountNumber;
+	private String password;
+	
+	public CardInformation() {}
+	public CardInformation(int id, int employeeNo, int cardNumber, String bank, int accountNumber, String password) {
+		this.id = id;
+		this.employeeNo = employeeNo;
+		this.cardNumber = cardNumber;
+		this.bank = bank;
+		this.accountNumber = accountNumber;
+		this.password = password;
+	}
+	
+	public int getId() { return id; }
+	public void setId(int id) { this.id = id; }
+	public int getEmployeeNo() { return employeeNo; }
+	public void setEmployeeNo(int employeeNo) { this.employeeNo = employeeNo; }
+	public int getCardNumber() { return cardNumber; }
+	public void setCardNumber(int cardNumber) { this.cardNumber = cardNumber; }
+	public String getBank() { return bank; }
+	public void setBank(String bank) { this.bank = bank; }
+	public int getAccountNumber() { return accountNumber; }
+	public void setAccountNumber(int accountNumber) { this.accountNumber = accountNumber; }
+	public String getPassword() { return password; }
+	public void setPassword(String password) { this.password = password; }
+}
