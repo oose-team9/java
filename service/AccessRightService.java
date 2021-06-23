ba46c8b2bb9e613aa9e964cf105a127f7c655a56
dyddn3994 <69083407+dyddn3994@users.noreply.github.com>
2021-06-23 17:24:08 +0900

    Update AccessRightService.java

--- service/AccessRightService.java
+++ service/AccessRightService.java
@@ -8,10 +8,10 @@ import persistence.AccessRightRepository;
 public class AccessRightService {
 	private final AccessRightRepository accessRightRepository = AccessRightRepository.getInstacne();
 	public AccessRightService() {}
-	public void write(AccessRight accessRight) {
+	public void write(AccessRight accessRight) throws Exception {
 		accessRightRepository.save(accessRight);
 	}
 	public ArrayList<AccessRight> findBoards() {
-        return accessRightRepository.findAll();
+        	return accessRightRepository.findAll();
     }
 }
